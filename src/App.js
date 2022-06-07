821e31a30e3e34dd33988a3329f70507909b375c
dariohimo <dariohimo@gmail.com>
2022-06-07 16:41:15 -0500

    add reyes eventos video youtube react para cristianos

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import imagen0 from './reyes/rey_atanagildo.png';
+import imagen1 from './reyes/rey_sisebuto.png';
+import { useRef } from 'react';
+
+import './App.css';
+
+function App() {
+  
+  const cambio = 23.16;
+  const refCaja = useRef();
+
+  const incrementar = (e) => {
+    e.target.innerHTML = Number(e.target.innerHTML) + 1;
+     
+    if(e.target.innerHTML >= 10){
+      e.target.innerHTML=1;
+     }
+     if(e.target.innerHTML >= 8){
+       e.target.style.backgroundColor = 'red'
+     } else {
+       e.target.style.backgroundColor="white";
+     }
+
+  }
+  
+  const convertir = () => {
+   
+    refCaja.current.innerHTML = Number(refCaja.current.innerHTML) * cambio; 
+  }
+  
+  const cambiar = (j) => {
+    
+    if(j.target.src.includes("atanagildo")) {
+      j.target.src = imagen1;
+  } else {
+      j.target.src= imagen0;
+  }
+  }
+
+  const lectura = (e) => {
+    refCaja.current.innerHTML = e.target.value;
+  }
+
+  return (
+
+    <>
+      <div ref={refCaja} className='caja' onClick={incrementar}>1 </div>
+         
+      <button onClick={convertir}> Convertir Peso </button>
+
+       <div> <img onClick={cambiar} src={imagen0} alt="rey"/> </div>
+
+      <input onChange={lectura} className='campo' />
+
+    </>
+  );
+}
+
+export default App;
